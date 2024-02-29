ab6348ddab4c36f5f46cdbd93df0973b76ce4f5e
Илья Тупицин <ilhatupitsin@yandex.ru>
2024-02-29 11:33:06 +0300

    конец

--- 29 февраля 2024 года задачи/Program.cs
+++ 29 февраля 2024 года задачи/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Security.Cryptography;
+
 Console.WriteLine("Введите ID работкинка!");
 int Id =Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите имя работкинка!");
@@ -11,7 +13,8 @@ ProductionWorkek productionWorkek = new ProductionWorkek(Name, Id, Shift, Work_r
 productionWorkek.Print();
 ShiftSupervisor shiftSupervisor = new ShiftSupervisor("Igor", 2, 23000, 30000);
 shiftSupervisor.Print();
-
+Customer customer = new Customer("Igor", "Gorodishe", "88005553535", 13, true);
+customer.Print2();
 class Employee
 {
     public string _name = "";
@@ -56,7 +59,7 @@ class ProductionWorkek : Employee
     public override void Print()
     {
         base.Print();
-        Console.WriteLine("смена: "+ Shift + ", ставка:"+ Work_rate);
+        Console.WriteLine("смена: "+ Shift + ", ставка: "+ Work_rate);
     }
 }
 
@@ -79,4 +82,37 @@ class ShiftSupervisor : Employee
     }
 }
 
+class Person
+{   string name;
+    string adres;
+    string telefon;
+    public string Name { get { return name; } set { name = value; } }   
+    public string Adres { get { return adres; } set {  adres = value; } }
+    public string Telefon { get {  return telefon; } set { telefon = value; } }
+    public Person(string name, string adres, string telefon)
+    {
+        Name = name;
+        Adres = adres;
+        Telefon = telefon;
+    }
+    public virtual void Print2()
+    { Console.WriteLine("Имя: "+Name + " адрес: " + Adres + " телефон: " + Telefon); }
+
+}
+class Customer : Person
+{
+    int nomer;
+    bool rass;
+    public int Nomer { get { return nomer; } set {  nomer = value; } }
+    bool Rass { get { return rass; } set {  rass = value; } }
+    public Customer(string name, string adres ,string telefon, int nomer, bool rass)
+        :base(name,adres,telefon)
+    {
+        Nomer = nomer;
+        Rass = rass;
+    }
+    public override void Print2()
+    { Console.WriteLine("Имя: " + Name + " номер клиента: " + Nomer + " телефон: " + Telefon +" адрес: "+Adres+" желание получать рассылку: "+Rass); }
+
+}
 
